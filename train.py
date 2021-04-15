295b73914445bbcfaff4ffaee81ea16dc14f1654
GloriaPietropolli <gloria.pietropolli@gmail.com>
2021-04-15 11:44:19 +0200

    first implementation of the code

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ Implementation of the training routine for the 3D CNN with GAN
 
 import os
 import torch
+import torch.nn as nn
 from torch.optim import Adam
 from IPython import display
 from discriminator import Discriminator
@@ -26,7 +27,8 @@ mean_value_pixel = torch.tensor(mean_value_pixel.reshape(1, 3, 1, 1, 1))
 
 # definitions of the hyperparameters
 alpha = 4e-4
-lr = 1e-3
+lr_c = 1e-3
+lr_d = 1e-3
 alpha = torch.tensor(alpha)
 epoch1 = 1000  # number of step for the first phase of training
 epoch2 = 1000  # number of step for the second phase of training
@@ -34,23 +36,24 @@ epoch3 = 1000  # number of step for the third phase of training
 hole_min_d, hole_max_d = 10, 100
 hole_min_h, hole_max_h = 10, 100
 hole_min_w, hole_max_w = 10, 100
-
+cn_input_size = 160
+ld_input_size = 96
 
 # PHASE 1
-# COMPLETION NETWORK is trained with the MSE loss for T_c iterations
+# COMPLETION NETWORK is trained with the MSE loss for T_c (=epoch1) iterations
 model_completion = CompletionN()
-optimizer_completion = Adam(model_completion.parameters(), lr=lr)
+optimizer_completion = Adam(model_completion.parameters(), lr=lr_c)
 for ep in range(epoch1):
     for training_x in train_dataset:
         mask = generate_input_mask(
             shape=(training_x.shape[0], 1, training_x.shape[2], training_x.shape[3], training_x.shape[4]),
             hole_size=(hole_min_d, hole_max_d, hole_min_h, hole_max_h, hole_min_w, hole_max_w))
-        training_x_masked = training_x - training_x * mask + mean_value_pixel * mask # mask the training tensor with
+        training_x_masked = training_x - training_x * mask + mean_value_pixel * mask  # mask the training tensor with
         # pixel containing the mean value
         input = torch.cat((training_x_masked, mask), dim=1)
         output = model_completion(input)
 
-        loss = completion_network_loss(training_x, output, mask)
+        loss = completion_network_loss(training_x, output, mask)  # MSE
 
         print(f"[PHASE1 : EPOCH]: {ep}, [LOSS]: {loss.item():.6f}")
         display.clear_output(wait=True)
@@ -59,4 +62,14 @@ for ep in range(epoch1):
         loss.backward()
         optimizer_completion.step()
 
+# PHASE 2
+# COMPLETION NETWORK is FIXED and DISCRIMINATORS are trained form scratch for T_d (=epoch2) iterations
 
+
+model_discriminator = Discriminator(loc_input_shape=(3, ld_input_size, ld_input_size, ld_input_size),
+                                    glo_input_shape=(3, cn_input_size, cn_input_size, cn_input_size))
+optimizer_discriminator = Adam(model_discriminator.parameters(), lr=lr_d)
+loss_discriminator = nn.BCELoss()
+for ep in range(epoch2):
+    for training_x in train_dataset:
+        pass
--- utils.py
+++ utils.py
@@ -8,7 +8,9 @@ def generate_input_mask(shape, hole_size, hole_area=None, number_holes=1):
     """
     shape = (B, C, D, H, W), C=1 bc it iis only necessary one channel
     hole_size = (hole_min_d, hole_max_d, hole_min_h, hole_max_h, hole_min_w, hole_max_w)
-    hole_area = (left_corner, depth, width, height)
+    hole_area = (left_corner, width, height, depth)
+                contains the area where the holes are generated
+                used as input value for the LOCAL DISCRIMINATOR
     number_holes = holes considered
     output = masked tensor of shape (N, C, D, H, W) with holes (denoted with channel value 1)
     """
@@ -22,11 +24,28 @@ def generate_input_mask(shape, hole_size, hole_area=None, number_holes=1):
         hole_h = random.randint(hole_size[2], hole_size[3])
         hole_w = random.randint(hole_size[4], hole_size[5])
         if hole_area:
-            pass  # later implement what happens if the area where to put the hole is fixed
-        offset_x = random.randint(0, mask_w - hole_w)
-        offset_y = random.randint(0, mask_h - hole_h)
-        offset_z = random.randint(0, mask_d - hole_d)
+            area_x_min, area_y_min, area_z_min = hole_area[0]
+            area_w, area_h, area_d = hole_area[1]
+            offset_x = random.randint(area_x_min, area_x_min + area_w - hole_w)
+            offset_y = random.randint(area_y_min, area_y_min + area_h - hole_h)
+            offset_z = random.randint(area_z_min, area_z_min + area_d - hole_d)
+        else:
+            offset_x = random.randint(0, mask_w - hole_w)
+            offset_y = random.randint(0, mask_h - hole_h)
+            offset_z = random.randint(0, mask_d - hole_d)
 
-        mask[i, :, offset_z:offset_z + hole_d, offset_y:offset_y+hole_h, offset_x:offset_x+hole_w] = 1.0
+        mask[i, :, offset_z:offset_z + hole_d, offset_y:offset_y + hole_h, offset_x:offset_x + hole_w] = 1.0
         return mask
 
+
+def generate_hole_area(size, mask_size):
+    """
+    total_size = total size of the area (W,H,D)
+    mask_size = size of the input mask
+    """
+    mask_w, mask_h, mask_d = mask_size
+    area_w, area_h, area_d = size
+    offset_x = random.randint(0, mask_w - area_w)
+    offset_y = random.randint(0, mask_h - area_h)
+    offset_z = random.randint(0, mask_d - area_d)
+    return ((offset_x, offset_y, offset_z), (area_w, area_h, area_d))
