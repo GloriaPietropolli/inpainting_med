2e9a5aed05db74ac9e782c240afc156613ebfe49
GloriaPietropolli <gloria.pietropolli@gmail.com>
2021-04-16 12:59:00 +0200

    first implementation of the code

--- datasets.py
+++ datasets.py
@@ -1,15 +1,49 @@
 """
 Routine for the creation of the parallelepiped that composed the training set
 """
-import torch
-import numpy as np
 import netCDF4 as nc
+import numpy as np
 import pandas as pd
+from netCDF4._netCDF4 import Dataset
+
 from index_float import list_float_total
 
 constant_latitude = 111  # 1° of latitude corresponds to 111 km
 constant_longitude = 111  # 1° of latitude corresponds to 111 km
 float_path = "../FLOAT_BIO/"
+
+
+def read_date_time(date_time):
+    date_time_decoded = ''
+    for i in range(0, 14):
+        new_digit = date_time[i].decode('UTF-8')
+        date_time_decoded += new_digit
+    year = np.int(date_time_decoded[0:4])
+    month = np.int(date_time_decoded[4:6])
+    day = np.int(date_time_decoded[6:8])
+    week = np.int(month * 4 + day / 7)
+    date_time_decoded = year + 0.01 * week
+    return date_time_decoded
+
+
+def create_list_date_time(years_consider):
+    """
+    Creation of a list containing date_time reference for training dataset
+    years_consider = (first year considered, last year considered)
+    interval_of_time = intervals within measurement are aggregated
+    """
+    first_year_considered, last_year_considered = years_consider
+    total_list = []
+    for year in np.arange(first_year_considered, last_year_considered):
+        lists = np.arange(year, year + 0.52, 0.01)
+        for i in range(len(lists)):
+            lists[i] = round(lists[i], 2)
+        lists = lists.tolist()
+        total_list = total_list + lists
+    return total_list
+
+
+list_data_time = create_list_date_time(2015, 2020)
 
 
 def create_box(batch, number_channel, lat, lon, depth, resolution):
@@ -34,6 +68,9 @@ def create_box(batch, number_channel, lat, lon, depth, resolution):
     return empty_parallelepiped
 
 
+list_parallelepiped = [create_box(1, 5, (36, 44), (2, 9), (0, 0.6), (12, 12, 0.01))]
+
+
 def insert_model_values():
     pass
 
@@ -42,10 +79,19 @@ def insert_sat_values():
     pass
 
 
-def insert_float_values(parallelepiped=None):
+def insert_float_values(lat_limits, lon_limits, depth_limits):
     """
-    Function that update the parallelepiped updating the voxel where the float info is avaiable
+    Function that update the parallelepiped updating the voxel where the float info is available
+    lat_limits = (lat_min, lat_max)
+    lon_limits = (lon_min, lon_max)
+    depth_limits = (depth_min, depth_max) in km
+    parallelepiped = parallelepiped to fill with float values
+    index_parallelepiped = represents the index in the input parallelepiped list, i.e. the week considered
+                           for example index_parallelepiped = 1 means we are considering first week of 2015
     """
+    lat_min, lat_max = lat_limits
+    lon_min, lon_max = lon_limits
+    depth_min, depth_max = depth_limits
     data = pd.read_csv(float_path + 'data/Float_Index.txt', header=None).to_numpy()[:, 0]
     list_data = []
     for i in data:
@@ -55,7 +101,21 @@ def insert_float_values(parallelepiped=None):
             if float_number == list_data[i][0:7]:
                 path_current_float = float_path + "data/" + list_data[i]
                 ds = nc.Dataset(path_current_float)
-    return print(ds)
+                time = read_date_time(ds['DATE_CREATION'][:].data)  # 2015.22
+                lat = ds['LATITUDE'][:].data
+                lon = ds['LONGITUDE'][:].data
+                depth = ds['PRES'][:].data
+                if lat_max > lat > lat_min:
+                    if lon_max > lon > lon_min:
+                        if depth_max > depth > depth_min:
+                            index = list_data_time.index(time)  # index input tensor considered, i.e. the one to update
+                            select_parallelepiped = list_parallelepiped[index]
+                            temp = ds['TEMP'][:].data  # channel1
+                            salinity = ds['PSAL'][:].data  # channel2
+                            doxy = ds['DOXY'][:].data  # channel3
+
+    return
+
 
 # box = create_box(1, 5, (36, 44), (2, 9), (0, 0.6), (12, 12, 0.01))
 # print(box.size())
---
+++ utils_2.py
@@ -0,0 +1,8 @@
+"""
+FIle for the definition of the functione necessary for the creation of the tensor input list
+"""
+
+import torch
+import numpy as np
+
+
