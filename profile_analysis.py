36962302cf17afb32d75743975392528eb8c08f9
GloriaPietropolli <gloria.pietropolli@gmail.com>
2022-01-21 10:22:34 +0100

    analysis script improvement

--- profile_analysis.py
+++ profile_analysis.py
@@ -16,11 +16,18 @@ from hyperparameter import latitude_interval, longitude_interval, depth_interval
 
 sns.set(context='notebook', style='whitegrid')
 
+epoch_float, lr_float = 25, 0.0001
+
+name_model = "model_completion_epoch_500_500_200_lrc_0.01_lrd_0.01"
+model_considered = 'model2015/' + name_model
+path_model = os.getcwd() + '/model/' + model_considered + '.pt'
+path_model_float = os.getcwd() + '/result2/' + name_model + '/' + str(epoch_float) + '/' + str(lr_float) + '/model.pt'
+
 dict_channel = {'temperature': 0, 'salinity': 1, 'oxygen': 2, 'chla': 3}
-dict_channel = {'temperature': 0, }
 
 for variable in list(dict_channel.keys()):
     snaperiod = 25
+    print("plotting " + variable + " vertical profile")
 
     constant_latitude = 111  # 1° of latitude corresponds to 111 km
     constant_longitude = 111  # 1° of latitude corresponds to 111 km
@@ -47,20 +54,6 @@ for variable in list(dict_channel.keys()):
     mean_value_pixel = MV_pixel(mvp_dataset)  # compute the mean of the channel of the training set
     mean_value_pixel = torch.tensor(mean_value_pixel.reshape(1, 4, 1, 1, 1))
 
-    epoch_float, lr_float = 25, 0.0001
-
-    # model_considered = 'model2015_c/model_completion_epoch_' + str(epoch_model) + '_lrc_' + str(lr_model)
-    where = 'model2015/'
-
-    if where == 'model2015/':
-        name_model = "model_completion_epoch_500_500_200_lrc_0.01_lrd_0.01"
-        # name_model = 'model_completion_epoch_500_500_200_lrc_0.01_lrd_0.01.pt_PLUS_epoch_100_lr_0.0001'
-        # name_model = 'model_completion_epoch_250_150_150_lrc_0.01_lrd_0.01'
-        model_considered = where + name_model
-        path_model = os.getcwd() + '/model/' + model_considered + '.pt'
-        path_model_float = os.getcwd() + '/result2/' + name_model + '/' + str(epoch_float) + '/' + str(
-            lr_float) + '/model.pt'
-
     model = CompletionN()
     model.load_state_dict(torch.load(path_model))  # network trained only with model information
     model.eval()
@@ -81,6 +74,10 @@ for variable in list(dict_channel.keys()):
     path_fig = path_fig + variable
     if not os.path.exists(path_fig):
         os.mkdir(path_fig)
+    if not os.path.exists(path_fig + '/mean/'):
+        os.mkdir(path_fig + '/mean/')
+    if not os.path.exists(path_fig + '/std/'):
+        os.mkdir(path_fig + '/std/')
 
     months = ["0" + str(month) for month in range(1, 10)] + [str(month) for month in range(10, 52)]
 
@@ -89,8 +86,9 @@ for variable in list(dict_channel.keys()):
             month = month[:-1]
             # month = "12"
         datetime = "2015." + month
-        data_tensor = os.getcwd() + '/tensor/model2015_n/datetime_' + str(datetime) + '.pt'  # get the data_tensor correspondent to the datetime of emodnet sample to feed the nn (
-            # NORMALIZED!!)
+        data_tensor = os.getcwd() + '/tensor/model2015_n/datetime_' + str(
+            datetime) + '.pt'  # get the data_tensor correspondent to the datetime of emodnet sample to feed the nn (
+        # NORMALIZED!!)
         data_tensor = torch.load(data_tensor)
 
         # TEST ON THE MODEL'S MODEL AND THE FLOAT MODEL WITH SAME HOLE
@@ -127,11 +125,13 @@ for variable in list(dict_channel.keys()):
             std_mod.append(torch.std(unkn_model))
             std_flo.append(torch.std(unkn_float))
 
-        mod = zip(means_mod, std_mod, means_flo, std_flo, means_phys, std_phys)
-        means_mod, std_mod, means_flo, std_flo, means_phys, std_phys = zip(*mod)
+        zip_result = zip(means_mod, std_mod, means_flo, std_flo, means_phys, std_phys)
+        zip_result = [x for x in zip_result if x[0] > 5]
+        if zip_result:
+            means_mod, std_mod, means_flo, std_flo, means_phys, std_phys = zip(*zip_result)
+        else:
+            continue
 
-        # plt.plot(means_phys, range(len(means_phys)), color="slategray", linestyle='--', marker='v', alpha=0.8,
-        # label="physical model")
         plt.plot(means_phys, color="slategray", linestyle='--', marker='v', alpha=0.8, label="physical model")
         plt.plot(means_mod, color="deeppink", linestyle='--', marker='o', alpha=0.8, label="CNN + GAN model")
         plt.plot(means_flo, color="purple", linestyle='--', marker='*', alpha=0.8, label="CNN + GAN + float")
@@ -140,7 +140,7 @@ for variable in list(dict_channel.keys()):
         plt.xlabel("depth")
         plt.suptitle("Week " + str(month))
         plt.title("Profile of the mean of " + variable)
-        plt.savefig(path_fig + '/' + variable + '_pro_mean_' + month + '.png')
+        plt.savefig(path_fig + '/mean/' + variable + '_pro_mean_' + month + '.png')
         plt.close()
 
         plt.plot(std_phys, color="slategray", linestyle='--', marker='v', alpha=0.8, label="physical model")
@@ -151,6 +151,5 @@ for variable in list(dict_channel.keys()):
         plt.xlabel("depth")
         plt.suptitle("Week " + str(month))
         plt.title("Profile of the std of " + variable)
-        plt.savefig(path_fig + '/' + variable + '_pro_std_' + month + '.png')
+        plt.savefig(path_fig + '/std/' + variable + '_pro_std_' + month + '.png')
         plt.close()
-
--- surface_analysis.py
+++ surface_analysis.py
@@ -16,6 +16,13 @@ from hyperparameter import latitude_interval, longitude_interval, depth_interval
 
 sns.set(context='notebook', style='whitegrid')
 
+epoch_float, lr_float = 25, 0.0001
+
+name_model = "model_completion_epoch_500_500_200_lrc_0.01_lrd_0.01"
+model_considered = 'model2015/' + name_model
+path_model = os.getcwd() + '/model/' + model_considered + '.pt'
+path_model_float = os.getcwd() + '/result2/' + name_model + '/' + str(epoch_float) + '/' + str(lr_float) + '/model.pt'
+
 dict_channel = {'temperature': 0, 'salinity': 1, 'oxygen': 2, 'chla': 3}
 dict_channel = {'temperature': 0, }
 
@@ -46,20 +53,6 @@ for variable in list(dict_channel.keys()):
     mvp_dataset, mean_model, std_model = Normalization(mvp_dataset)
     mean_value_pixel = MV_pixel(mvp_dataset)  # compute the mean of the channel of the training set
     mean_value_pixel = torch.tensor(mean_value_pixel.reshape(1, 4, 1, 1, 1))
-
-    epoch_float, lr_float = 25, 0.0001
-
-    # model_considered = 'model2015_c/model_completion_epoch_' + str(epoch_model) + '_lrc_' + str(lr_model)
-    where = 'model2015/'
-
-    if where == 'model2015/':
-        name_model = "model_completion_epoch_500_500_200_lrc_0.01_lrd_0.01"
-        # name_model = 'model_completion_epoch_500_500_200_lrc_0.01_lrd_0.01.pt_PLUS_epoch_100_lr_0.0001'
-        # name_model = 'model_completion_epoch_250_150_150_lrc_0.01_lrd_0.01'
-        model_considered = where + name_model
-        path_model = os.getcwd() + '/model/' + model_considered + '.pt'
-        path_model_float = os.getcwd() + '/result2/' + name_model + '/' + str(epoch_float) + '/' + str(
-            lr_float) + '/model.pt'
 
     model = CompletionN()
     model.load_state_dict(torch.load(path_model))  # network trained only with model information
@@ -125,16 +118,16 @@ for variable in list(dict_channel.keys()):
         std_mod.append(torch.std(unkn_model))
         std_flo.append(torch.std(unkn_float))
 
-    mod = zip(means_mod, std_mod, means_flo, std_flo, means_phys, std_phys)
-    mod = [x for x in mod if x[0] > 5]
-    means_mod, std_mod, means_flo, std_flo, means_phys, std_phys = zip(*mod)
+    zip_result = zip(means_mod, std_mod, means_flo, std_flo, means_phys, std_phys)
+    zip_result = [x for x in zip_result if x[0] > 8]
+    means_mod, std_mod, means_flo, std_flo, means_phys, std_phys = zip(*zip_result)
 
     plt.plot(means_phys, color="slategray", linestyle='--', marker='v', alpha=0.8, label="physical model")
     plt.plot(means_mod, color="deeppink", linestyle='--', marker='o', alpha=0.8, label="CNN + GAN model")
     plt.plot(means_flo, color="purple", linestyle='--', marker='*', alpha=0.8, label="CNN + GAN + float")
     plt.legend()
     plt.ylabel(variable)
-    plt.suptitle("mean")
+    plt.suptitle("MEAN")
     plt.title("Surface time series of the " + variable)
     plt.savefig(path_fig + '/' + variable + '_ts_mean.png')
     plt.show()
@@ -145,7 +138,7 @@ for variable in list(dict_channel.keys()):
     plt.plot(std_flo, color="purple", linestyle='--', marker='*', alpha=0.8, label="CNN + GAN + float")
     plt.legend()
     plt.ylabel(variable)
-    plt.suptitle("std")
+    plt.suptitle("STD")
     plt.title("Surface time series of the " + variable)
     plt.savefig(path_fig + '/' + variable + '_ts_std.png')
     plt.show()
